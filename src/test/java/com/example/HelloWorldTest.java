b0073171a5af564da0c2883eb3196153dfd6ad53
MXLee21 <lmxlee41@gmail.com>
2024-08-12 16:21:27 +0800

    modify java file2

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -2,10 +2,11 @@ package com.example;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 public class HelloWorldTest {
     @Test
     public void testGetGreeting() {
         HelloWorld helloWorld = new HelloWorld();
-        assertEquals("Hello, World!", helloWorld.getGreeting());
+        assertEquals("Hello, World!nnnn", helloWorld.getGreeting());
     }
 }
